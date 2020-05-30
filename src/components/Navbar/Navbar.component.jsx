798d6e6aad44dbd2f5a6c7aa0150a70cc41a2db6
oluSammy <olumorinsammy@gmail.com>
2020-05-30 02:56:15 -0700

    initial comiit, created component folders

---
+++ src/components/Navbar/Navbar.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navbar = () => (
+    <nav>
+        <div class="nav-wrapper">
+        <div className="container">
+            <a href="#" class="brand-logo">Logo</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li>
+                <li><a href="collapsible.html">JavaScript</a></li>
+            </ul>
+            </div>
+        </div>
+  </nav>
+);
+
+export default Navbar;
